2cc4ab92d53b784a29a5b3fe120eccb66ba7283c
Coursera Learner <nobody@nowhere.nul>
2019-06-30 10:25:26 +0000

    retirement done

---
+++ 07_retirement/retirement.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define PERIOD 12
+
+struct _retire_info {
+  int months;
+  double  contribution;
+  double rate_of_return;
+};
+
+typedef struct _retire_info retire_info;  
+
+double updateBalance (double balance, retire_info info) {
+  double updatedBal = balance * info.rate_of_return + info.contribution;
+  return updatedBal;
+}
+  
+void retirement (int startAge,   //in months
+		 double initial, //initial savings in dollars
+		 retire_info working, //info about working
+		 retire_info retired){ //info about being retired 
+
+  int retireAge = startAge + working.months;
+  double updatedBal =  initial;
+  for (int m = startAge; m<retireAge; m++){
+    // update balance
+    printf("Age %3d month %2d you have $%.2lf\n", m/PERIOD, m, updatedBal);
+    updatedBal =  updateBalance (updatedBal, working);
+  }
+
+  for(int m = retireAge; m < retireAge + retired.months; m++) {
+    printf("Age %3d month %2d you have $%.2lf\n", m/PERIOD, m, updatedBal);
+    updatedBal = updateBalance(updatedBal, retired);
+  }
+
+}
+
+int main(void) {
+  retire_info working = {489, 1000, 0.045 / PERIOD};
+  retire_info retired = {384, -4000, 0.01 / PERIOD};
+
+  retirement(327, 21345, working, retired);
+
+  return 0;
+}
